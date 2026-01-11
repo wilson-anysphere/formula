64389431dc5e99e82577189f47a43dae03df029b
root <root@ip-10-140-21-115.ec2.internal>
2026-01-10 18:23:25 -0800

    chore(spreadsheet-frontend): re-export EngineCellCacheOptions

--- packages/spreadsheet-frontend/src/index.ts
+++ packages/spreadsheet-frontend/src/index.ts
@@ -2,7 +2,7 @@ export type { Range0 } from "./a1";
 export { colToName, fromA1, range0ToA1, toA1 } from "./a1";
 
 export { EngineCellCache } from "./cache";
+export type { EngineCellCacheOptions } from "./cache";
 
 export type { EngineGridProviderOptions } from "./grid-provider";
 export { EngineGridProvider } from "./grid-provider";
-
