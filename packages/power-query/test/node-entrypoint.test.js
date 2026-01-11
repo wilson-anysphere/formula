4cd44a4c1d232af3fc0e1725c0c80c22a929a1c4
root <root@ip-10-140-20-78.ec2.internal>
2026-01-11 07:42:51 -0800

    test(power-query): cover node entrypoint exports
    Adds a small regression test ensuring  exposes Node-only helpers like EncryptedFileSystemCacheStore and createNodeCredentialStore.

---
+++ packages/power-query/test/node-entrypoint.test.js
@@ -0,0 +1,11 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+
+import * as powerQuery from "../src/node.js";
+
+test("power-query node entrypoint exports Node-only helpers", () => {
+  assert.equal(typeof powerQuery.FileSystemCacheStore, "function");
+  assert.equal(typeof powerQuery.EncryptedFileSystemCacheStore, "function");
+  assert.equal(typeof powerQuery.createNodeCredentialStore, "function");
+});
+
