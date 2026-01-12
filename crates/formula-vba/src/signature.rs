5c1426fc3579f6fdb036d044d6990c5dcb2ccca1
Wilson Lin <code@wilsonl.in>
2026-01-12 09:02:50 -0800

    chore(formula-vba): drop unused DigestAlg import

--- crates/formula-vba/src/signature.rs
+++ crates/formula-vba/src/signature.rs
@@ -4,7 +4,6 @@ use crate::{
     authenticode::extract_vba_signature_signed_digest,
     compute_vba_project_digest_v3,
     contents_hash::content_normalized_data,
-    DigestAlg,
     normalized_data::forms_normalized_data,
     OleError,
     OleFile,
