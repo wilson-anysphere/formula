25bfcccc196be2f2138bf1a04ae2a9a924edf7b6
Wilson Lin <code@wilsonl.in>
2026-01-11 23:10:42 -0800

    chore(desktop): re-export mountTitlebar handle types

--- apps/desktop/src/titlebar/index.ts
+++ apps/desktop/src/titlebar/index.ts
@@ -1,3 +1,4 @@
 export { Titlebar } from "./Titlebar.js";
 export type { TitlebarAction, TitlebarProps } from "./Titlebar.js";
 export { mountTitlebar } from "./mountTitlebar.js";
+export type { MountedTitlebar, TitlebarDisposer } from "./mountTitlebar.js";
