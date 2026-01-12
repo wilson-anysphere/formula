9a6fda0c4f51a4a7e23637bb3b385506e25214c0
Wilson Lin <code@wilsonl.in>
2026-01-12 13:43:55 -0800

    test(formula-engine): exercise bytecode parser for lambda lookup cases

--- crates/formula-engine/tests/bytecode_lambda_lookup.rs
+++ crates/formula-engine/tests/bytecode_lambda_lookup.rs
@@ -1,7 +1,7 @@
 #![cfg(not(target_arch = "wasm32"))]
 
 use formula_engine::bytecode::{
-    self, ast::Function, Array, CellCoord, ColumnarGrid, ErrorKind, Expr, Ref, RangeRef, Value, Vm,
+    self, ast::Function, Array, CellCoord, ColumnarGrid, ErrorKind, Expr, Value, Vm,
 };
 use std::sync::Arc;
 
@@ -11,27 +11,6 @@ fn identity_lambda() -> Expr {
         params: Arc::from(vec![param.clone()].into_boxed_slice()),
         body: Box::new(Expr::NameRef(param)),
     }
-}
-
-fn range_a1_a2() -> Expr {
-    Expr::RangeRef(RangeRef::new(
-        Ref::new(0, 0, false, false),
-        Ref::new(1, 0, false, false),
-    ))
-}
-
-fn range_b1_b2() -> Expr {
-    Expr::RangeRef(RangeRef::new(
-        Ref::new(0, 1, false, false),
-        Ref::new(1, 1, false, false),
-    ))
-}
-
-fn range_a1_b2() -> Expr {
-    Expr::RangeRef(RangeRef::new(
-        Ref::new(0, 0, false, false),
-        Ref::new(1, 1, false, false),
-    ))
 }
 
 fn array_a1_b2() -> Expr {
@@ -50,14 +29,7 @@ fn array_a1_b2() -> Expr {
 #[test]
 fn bytecode_match_rejects_lambda_lookup_value() {
     let origin = CellCoord::new(0, 0);
-    let expr = Expr::FuncCall {
-        func: Function::Match,
-        args: vec![
-            identity_lambda(),
-            range_a1_a2(),
-            Expr::Literal(Value::Number(0.0)),
-        ],
-    };
+    let expr = bytecode::parse_formula("=MATCH(LAMBDA(x,x),A1:A2,0)", origin).expect("parse");
     let program = bytecode::Compiler::compile(Arc::from("match_lambda_lookup_value"), &expr);
 
     let grid = ColumnarGrid::new(10, 10);
@@ -70,10 +42,7 @@ fn bytecode_match_rejects_lambda_lookup_value() {
 #[test]
 fn bytecode_xmatch_rejects_lambda_lookup_value() {
     let origin = CellCoord::new(0, 0);
-    let expr = Expr::FuncCall {
-        func: Function::XMatch,
-        args: vec![identity_lambda(), range_a1_a2()],
-    };
+    let expr = bytecode::parse_formula("=XMATCH(LAMBDA(x,x),A1:A2)", origin).expect("parse");
     let program = bytecode::Compiler::compile(Arc::from("xmatch_lambda_lookup_value"), &expr);
 
     let grid = ColumnarGrid::new(10, 10);
@@ -86,10 +55,8 @@ fn bytecode_xmatch_rejects_lambda_lookup_value() {
 #[test]
 fn bytecode_xlookup_rejects_lambda_lookup_value() {
     let origin = CellCoord::new(0, 0);
-    let expr = Expr::FuncCall {
-        func: Function::XLookup,
-        args: vec![identity_lambda(), range_a1_a2(), range_b1_b2()],
-    };
+    let expr =
+        bytecode::parse_formula("=XLOOKUP(LAMBDA(x,x),A1:A2,B1:B2)", origin).expect("parse");
     let program = bytecode::Compiler::compile(Arc::from("xlookup_lambda_lookup_value"), &expr);
 
     let grid = ColumnarGrid::new(10, 10);
@@ -102,15 +69,8 @@ fn bytecode_xlookup_rejects_lambda_lookup_value() {
 #[test]
 fn bytecode_vlookup_rejects_lambda_lookup_value() {
     let origin = CellCoord::new(0, 0);
-    let expr = Expr::FuncCall {
-        func: Function::VLookup,
-        args: vec![
-            identity_lambda(),
-            range_a1_b2(),
-            Expr::Literal(Value::Number(1.0)),
-            Expr::Literal(Value::Bool(false)),
-        ],
-    };
+    let expr =
+        bytecode::parse_formula("=VLOOKUP(LAMBDA(x,x),A1:B2,2,FALSE)", origin).expect("parse");
     let program = bytecode::Compiler::compile(Arc::from("vlookup_lambda_lookup_value"), &expr);
 
     let grid = ColumnarGrid::new(10, 10);
@@ -123,15 +83,8 @@ fn bytecode_vlookup_rejects_lambda_lookup_value() {
 #[test]
 fn bytecode_hlookup_rejects_lambda_lookup_value() {
     let origin = CellCoord::new(0, 0);
-    let expr = Expr::FuncCall {
-        func: Function::HLookup,
-        args: vec![
-            identity_lambda(),
-            range_a1_b2(),
-            Expr::Literal(Value::Number(1.0)),
-            Expr::Literal(Value::Bool(false)),
-        ],
-    };
+    let expr =
+        bytecode::parse_formula("=HLOOKUP(LAMBDA(x,x),A1:B2,2,FALSE)", origin).expect("parse");
     let program = bytecode::Compiler::compile(Arc::from("hlookup_lambda_lookup_value"), &expr);
 
     let grid = ColumnarGrid::new(10, 10);
