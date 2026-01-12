18efca6a9233e6e4c38eb17a99fc6eb596ae52af
Wilson Lin <code@wilsonl.in>
2026-01-11 23:54:13 -0800

    feat(versioning): add lightweight formula entrypoint
    Add packages/versioning/src/formula/index.js to export formula utilities (normalize/parse/tokenize/diff) without importing heavy version store modules.

---
+++ packages/versioning/src/formula/index.js
@@ -0,0 +1,5 @@
+export { normalizeFormula, normalizeFormulaText } from "./normalize.js";
+export { parseFormula } from "./parse.js";
+export { tokenizeFormula } from "./tokenize.js";
+export { diffFormula } from "./diff.js";
+
