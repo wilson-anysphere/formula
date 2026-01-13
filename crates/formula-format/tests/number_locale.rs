65e5da21f8b90817e814b236fe662e78688bbff2
Wilson Lin <code@wilsonl.in>
2026-01-13 14:53:32 -0800

    test(formula-format): cover fr-CH/it-CH NumberLocale aliases

--- crates/formula-format/tests/number_locale.rs
+++ crates/formula-format/tests/number_locale.rs
@@ -31,6 +31,14 @@ fn formats_thousands_grouping_for_each_locale() {
         "1'234'567.5"
     );
     assert_eq!(
+        locale::format_number(value, locale::get_locale("fr-CH").unwrap()),
+        "1'234'567.5"
+    );
+    assert_eq!(
+        locale::format_number(value, locale::get_locale("it-CH").unwrap()),
+        "1'234'567.5"
+    );
+    assert_eq!(
         locale::format_number(value, locale::get_locale("fr-FR").unwrap()),
         format!("1{nbsp}234{nbsp}567,5")
     );
@@ -84,8 +92,10 @@ fn get_locale_normalizes_common_locale_id_spellings() {
     assert_eq!(locale::get_locale("en_gb").unwrap().id, "en-GB");
     assert_eq!(locale::get_locale("de_ch").unwrap().id, "de-CH");
     assert_eq!(locale::get_locale("fr_fr").unwrap().id, "fr-FR");
+    assert_eq!(locale::get_locale("fr_ch").unwrap().id, "fr-CH");
     assert_eq!(locale::get_locale("es").unwrap().id, "es-ES");
     assert_eq!(locale::get_locale("it").unwrap().id, "it-IT");
+    assert_eq!(locale::get_locale("it_CH").unwrap().id, "it-CH");
 }
 
 #[test]
