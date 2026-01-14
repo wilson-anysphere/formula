857accb95eed5228f40e8ef28230e1fbb42f4838
Wilson Lin <code@wilsonl.in>
2026-01-14 00:07:19 -0800

    test(collab-binder): ignore applyState DocumentController events

--- packages/collab/binder/test/binder-document-change-source-filter.test.js
+++ packages/collab/binder/test/binder-document-change-source-filter.test.js
@@ -89,3 +89,38 @@ test("binder ignores DocumentController change events with source=collab (preven
   }
 });
 
+test("binder ignores DocumentController change events with source=applyState (prevents snapshot restore from overwriting Yjs)", async () => {
+  const doc = new Y.Doc();
+  const { sheets } = getWorkbookRoots(doc);
+
+  doc.transact(() => {
+    const sheet = new Y.Map();
+    sheet.set("id", "Sheet1");
+    sheet.set("name", "Sheet1");
+    sheets.push([sheet]);
+  });
+
+  const documentController = new TestDocumentController();
+  const binder = bindYjsToDocumentController({ ydoc: doc, documentController, defaultSheetId: "Sheet1" });
+
+  try {
+    let updateCount = 0;
+    doc.on("update", () => {
+      updateCount += 1;
+    });
+
+    await flushAsync(5);
+    updateCount = 0;
+
+    const before = documentController.getSheetView("Sheet1");
+    const after = { ...before, frozenRows: 2 };
+    documentController.applyExternalSheetViewDeltas([{ sheetId: "Sheet1", before, after }], { source: "applyState" });
+
+    await flushAsync(5);
+
+    assert.equal(updateCount, 0);
+  } finally {
+    binder.destroy();
+    doc.destroy();
+  }
+});
